25ac5ee62ae2c2d8c6acdaadb065b7fc22166001
faizalindra <faizalindra9@gmail.com>
2021-07-01 12:49:24 +0700

    Cleaning Home + BookingPage

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -10,30 +10,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  // int _selectedIndex = 0;
-  // static const TextStyle optionStyle =
-  //     TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  // static const List<Widget> _widgetOptions = <Widget>[
-  // Text(
-  //   'Index 0: Home',
-  //   style: optionStyle,
-  // ),
-  // Text(
-  //   'Index 1: Business',
-  //   style: optionStyle,
-  // ),
-  // Text(
-  //   'Index 2: School',
-  //   style: optionStyle,
-  // ),
-  // ];
-
-  // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //   });
-  // }
-
   final List<Widget> _screens = [
     HomeScreen(),
     JamBooking(),
@@ -47,25 +23,7 @@ class _HomePageState extends State<HomePage> {
   int _activeScreenIndex = 0;
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Floating Action Button Label'),
-      // ),
       body: _screens[_activeScreenIndex],
-      // body: SingleChildScrollView(
-      //   child: Container(
-      //       child: Padding(
-      //           padding: const EdgeInsets.all(8.0),
-      //           child: Column(
-      //             children: [
-      //               logoHome(),
-      //               SizedBox(height: 24.0),
-      //               lapanganA(),
-      //               SizedBox(height: 24.0),
-      //               SizedBox(height: 24.0),
-      //               // tombolNav()
-      //             ],
-      //           ))),
-      // ),
 
       //Tombol Booking
       floatingActionButton: FloatingActionButton.extended(
@@ -87,23 +45,6 @@ class _HomePageState extends State<HomePage> {
             _activeScreenIndex = index;
           });
         },
-        //   items: <BottomNavigationBarItem>[
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.home),
-        //       label: 'Home',
-        //       ),
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.access_time,
-        //       label: 'Jam Booking',
-        //       ),
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.book_online),
-        //       label: 'Daftar Booking',
-        //       ),
-        //   ],
-        // ),
-
-        // bottomNavigationBar: BottomNavigationBar(
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
@@ -118,9 +59,6 @@ class _HomePageState extends State<HomePage> {
             label: 'Booking List',
           ),
         ],
-        //   currentIndex: _selectedIndex,
-        //   selectedItemColor: Colors.amber[800],
-        //   onTap: _onItemTapped,
       ),
 
       //side bar
@@ -144,6 +82,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
+//Widget Lapangan
 Widget lapanganA() {
   return new Container(
     padding: EdgeInsets.symmetric(vertical: 20, horizontal: 100),
@@ -151,13 +90,6 @@ Widget lapanganA() {
     decoration: BoxDecoration(
       color: Colors.lightBlue[50],
       borderRadius: BorderRadius.circular(10),
-      // child: Row(
-      //   children: [
-      //     Text('Lapangan A'),
-      //     Text('Harga Rp. 160.000/jam'),
-      //     Text('Rumput Sintesis'),
-      //   ],
-      // ),
     ),
     child: Column(
       children: [
